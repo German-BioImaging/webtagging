9636fcaa810bd7cb8d3075f095920daaedc9566e
dpwrussell <douglas.russell@bioch.ox.ac.uk>
2014-01-07 16:32:40 +0000

    Updated to support django-1.5+ and

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
-
 # This settings.py file will be imported by omero.settings file AFTER it has initialised custom settings.
-from django.conf import settings
+import django
 
-# We can directly manipulate the settings
-# E.g. add plugins to RIGHT_PLUGINS list
-settings.CENTER_PLUGINS.append(["Auto Tag", "webtagging/auto_tag_init.js.html", "auto_tag_panel"])
+# Old style
+if django.get_version < 1.6:
+    from django.conf import settings
+
+    # We can directly manipulate the settings
+    # E.g. add plugins to RIGHT_PLUGINS list
+    settings.CENTER_PLUGINS.append(["Auto Tag", "webtagging/auto_tag_init.js.html", "auto_tag_panel"])
